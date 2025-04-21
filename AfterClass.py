2475cf7d47b446793b4bb6840a8444a235e714ac
NiranjanaAneesh <niranjanaaneeessh@gmail.com>
2025-04-21 21:18:54 +0530

    howdy

---
+++ AfterClass.py
@@ -0,0 +1,10 @@
+#Take a number as input from user
+ABCD = int(input("Please Enter a number to find the square root of :"))
+
+#Finding square root
+Sqroot = ABCD**0.5
+
+#Presenting square root
+print("The square root of",ABCD)
+print("is:",Sqroot)
+
